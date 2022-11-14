6d25604e0ad82bdeb15b9c89810b6631512fbfda
Lavanya7987 <117310907+Lavanya7987@users.noreply.github.com>
2022-11-14 22:28:34 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-/* 
-Node On Docker
-*/
+'use strict'
+const express = require ('express')
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express()
+app.get('/', (req,res) => {
+    res.send('<h2 style="color: purple">Hello World!<h2>');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
